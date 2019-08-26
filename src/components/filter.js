3daa7f7d53d06c1c919b41d54801b491f7a8d377
Dima <dima@onmaster.ru>
2019-08-26 11:13:02 +0300

    Пробую стейты внести в классы

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,13 +1,12 @@
 import AbstractComponent from "./abstract-component";
 
-const filterState = {
-  active: `everything`,
-};
-
 export default class Filters extends AbstractComponent {
   constructor(items) {
     super();
     this._items = items;
+    this._filterState = {
+      active: `everything`,
+    };
   }
 
   /**
@@ -18,9 +17,9 @@ export default class Filters extends AbstractComponent {
    *
    * @return {string} HTML-код
    */
-  static getMarkupFilter([filterId, name]) {
+  _getMarkupFilter([filterId, name]) {
     return `<div class="trip-filters__filter">
-              <input id="filter-${filterId}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filterId}" ${filterId === filterState[`active`] ? `checked` : ``}>
+              <input id="filter-${filterId}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filterId}" ${filterId === this._filterState[`active`] ? `checked` : ``}>
               <label class="trip-filters__filter-label" for="filter-${filterId}">
                 ${name}
               </label>
@@ -29,7 +28,7 @@ export default class Filters extends AbstractComponent {
 
   getTemplate() {
     return `<form class="trip-filters" action="#" method="get">
-              ${this._items.map((it) => Filters.getMarkupFilter(it)).join(``)}
+              ${this._items.map((it) => this._getMarkupFilter(it)).join(``)}
               <button class="visually-hidden" type="submit">
                 Accept filter
               </button>
