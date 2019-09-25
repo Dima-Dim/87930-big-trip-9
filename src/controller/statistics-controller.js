4e94e7074a173a4868721acd610b316279751ed4
Dima <dima@onmaster.ru>
2019-09-25 10:13:30 +0300

    Добавил статистику по времени, проведённом в точках.

--- src/controller/statistics-controller.js
+++ src/controller/statistics-controller.js
@@ -35,6 +35,7 @@ export default class StatisticsController {
     };
     this._parseAllData(data);
     this._makeMoneyChart();
+    this._makeTimeChart();
     this._statisics.show();
   }
 
@@ -126,5 +127,58 @@ export default class StatisticsController {
     }
 
     return this._charts.money;
+  }
+
+  _makeTimeChart() {
+    const sourceDataForChart = this._getMoneyChartData(this._parsedData.time);
+    const canvas = this._statisics.getElement().querySelector(`.${ClassesElements.STATISTICS_TIME}`);
+    const ctxTags = canvas.getContext(`2d`);
+    ctxTags.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (this._charts.has(`time`)) {
+      updateChart(this._charts.get(`time`), sourceDataForChart[0], sourceDataForChart[1]);
+    } else {
+      const timeChart = new Chart(ctxTags, {
+        type: `horizontalBar`,
+        plugins: [ChartDataLabels],
+        data: {
+          labels: sourceDataForChart[0],
+          datasets: [{
+            data: sourceDataForChart[1],
+            backgroundColor: [
+              `rgba(255, 99, 132, 0.3)`,
+              `rgba(54, 162, 235, 0.3)`,
+              `rgba(255, 206, 86, 0.3)`,
+              `rgba(75, 192, 192, 0.3)`,
+              `rgba(153, 102, 255, 0.3)`,
+              `rgba(255, 159, 64, 0.3)`,
+            ],
+            borderColor: [
+              `rgba(255, 99, 132, 1)`,
+              `rgba(54, 162, 235, 1)`,
+              `rgba(255, 206, 86, 1)`,
+              `rgba(75, 192, 192, 1)`,
+              `rgba(153, 102, 255, 1)`,
+              `rgba(255, 159, 64, 1)`,
+            ],
+            borderWidth: 1
+          }],
+        },
+        options: {
+          plugins: {
+            datalabels: {
+              anchor: `end`,
+              align: `end`,
+              offset: -50,
+              formatter: (value) => `${value}H`
+            },
+          }
+        }
+      });
+
+      this._charts.set(`time`, timeChart);
+    }
+
+    return this._charts.time;
   }
 }
